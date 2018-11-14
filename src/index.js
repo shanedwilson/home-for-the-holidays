c836fbf074cf837558c2e86097a636317fa1fa7c
Shane D Wilson <me@shanedwilson.com>
2018-11-13 21:22:07 -0600

    working

--- src/index.js
+++ src/index.js
@@ -5,12 +5,14 @@ import apiKeys from '../db/apiKeys.json';
 
 import navbar from './components/Navbar/navbar';
 import auth from './components/Auth/auth';
+import authHelpers from './helpers/authHelpers';
 
 import './index.scss';
 
 const initApp = () => {
   firebase.initializeApp(apiKeys.firebaseKeys);
   navbar.createNavbar();
+  authHelpers.checkLoginStatus();
   auth.loginButton();
 };
 
